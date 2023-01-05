9dac52321390945a1242d8bce64f5b43c64b3778
nguyen nguyen <kaidragon2x@gmail.com>
2023-01-05 23:45:10 +0700

    upload source code

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// SHOW MENU
+const showMenu = (toggleId, navbarId,bodyId) =>{
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId)
+
+    if(toggle && navbar){
+        toggle.addEventListener('click', ()=>{
+            // APARECER MENU
+            navbar.classList.toggle('show')
+            // ROTATE TOGGLE
+            toggle.classList.toggle('rotate')
+            // PADDING BODY
+            bodypadding.classList.toggle('expander')
+        })
+    }
+}
+showMenu('nav-toggle','navbar','body')
+
+// LINK ACTIVE COLOR
+const linkColor = document.querySelectorAll('.nav__link');   
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink));
+
+
+
+
+// angularJs
+var app = angular.module("indexApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    
+    $routeProvider.when("/user", {
+        templateUrl: "container/user.html"
+    });
+});
+
+
+
+app.controller("indexCtrl", function ($scope) {
+
+});
